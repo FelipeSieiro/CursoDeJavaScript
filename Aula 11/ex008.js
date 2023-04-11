aa8fdb0c925b63ac37c5d71ab1c78ef89267346d
FelipeSieiro <felipedossantos0804@gmail.com>
2023-04-11 15:24:19 -0300

    Aula 11 Concluida.

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A VELOCIDADE DO SEU CARRO É DE ${vel} Km/h`)
+ if(vel>60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO! ')
+ }
+ console.log('dirija sempre com cinto de segurança')
